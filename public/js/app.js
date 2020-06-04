3566cdf1bb8fb9b485d722a337e72ff089afa18c
KrishnaPermal <Krishna.tantalidespermalnaiken@gmail.com>
2020-06-04 09:27:18 +0400

    remodif page accueil

--- public/js/app.js
+++ public/js/app.js
@@ -78557,8 +78557,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/Nouveau dossier/memoireNumerique_ver2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Nouveau dossier/memoireNumerique_ver2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/cyberun-2/projet/Projet/Bonne Version memoireNumerique_ver2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/cyberun-2/projet/Projet/Bonne Version memoireNumerique_ver2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
